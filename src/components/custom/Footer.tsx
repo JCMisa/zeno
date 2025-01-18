98b2c37744a4679d2d8be84dc2d8a9e93b3cd835
MisaJC <johncarlomisa399@gmail.com>
2025-01-18 15:06:08 +0800

    build the theme selector component

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -1,4 +1,4 @@
-import { Blocks } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 function Footer() {
@@ -8,7 +8,13 @@ function Footer() {
       <div className="max-w-7xl mx-auto px-4 py-8">
         <div className="flex flex-col md:flex-row items-center justify-between gap-4">
           <div className="flex items-center gap-2 text-gray-400">
-            <Blocks className="size-5" />
+            <Image
+              src={"/logo.svg"}
+              alt="logo"
+              width={1000}
+              height={1000}
+              className="size-5"
+            />
             <span>Built for developers, by developers</span>
           </div>
           <div className="flex items-center gap-6">
